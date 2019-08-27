07ec5345ebc7195ba747927ff9bd9fcbecd5249e
Matheus Costa <matheusgarciabsb@gmail.com>
2019-08-27 18:05:00 -0300

    Projeto criado

---
+++ src/test/java/com/matheuscosta/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscosta.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
